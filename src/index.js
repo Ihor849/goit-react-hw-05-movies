fa7e7cc0d5b05b92d9207e252ed8290d770adf0d
Ihor Marchenko <109768648+Ihor849@users.noreply.github.com>
2023-05-22 23:20:10 +0300

    rout

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="homework-dz-new">
+    <BrowserRouter basename="goit-react-hw-05-movies">
       <App />
     </BrowserRouter >
     <GlobalStyle />
